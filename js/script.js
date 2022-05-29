23ba67641de7fc53492532c5dc64bcd27d5ebd40
Max Ulanov <ulanovmaks29@gmail.com>
2022-05-29 15:53:40 +0300

    working with style

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  const swiper = new Swiper('.team_slider', {
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true
+    },
+    slidesPerView: 3,
+    spaceBetween: 20,
+    
+  })
+
+
+
+
+
+})
