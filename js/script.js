a9919dd759e649098d84afcb1457c613f206357e
Max Ulanov <ulanovmaks29@gmail.com>
2022-05-29 19:41:30 +0300

    responsive

--- js/script.js
+++ js/script.js
@@ -2,14 +2,20 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   const swiper = new Swiper('.team_slider', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
     scrollbar: {
       el: ".swiper-scrollbar",
       draggable: true
     },
-    slidesPerView: 3,
-    spaceBetween: 20,
-    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   })
+
+  const select1 = new CustomSelect('#select_job');
 
 
 
